e0c7ac812c2e21403dd57f6d8e3e6c253a71791c
Janis Karklins <janis.karklins88@gmail.com>
2026-07-03 17:42:42 +0300

    Update Account, update transferService

--- src/main/java/com/example/banking/service/TransferService.java
+++ src/main/java/com/example/banking/service/TransferService.java
@@ -20,14 +20,14 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class TransferService {
-  private final AccountRepository accountRepository;
+  private final AccountService accountService;
   private final TransactionRepository transactionRepository;
 
   public synchronized TransferResponseDTO transfer(TransferRequestDTO request) {
-    Account accountFrom = accountRepository.findById(request.fromAccountId())
+    Account accountFrom = accountService.getAccount(request.fromAccountId())
         .orElseThrow(() -> new ResourceNotFoundException("Source account not found"));
 
-    Account accountTo = accountRepository.findById(request.toAccountId())
+    Account accountTo = accountService.getAccount(request.toAccountId())
         .orElseThrow(() -> new ResourceNotFoundException("Destination account not found"));
 
     if (accountFrom.getBalance().compareTo(request.amount()) < 0) {
